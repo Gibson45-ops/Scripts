042f061115488156be82da86a3fbe5a986515de0
Gibson45-ops <trost.jack1@gmail.com>
2025-03-04 16:40:38 -0600

    Update AddMemberstoADGroup.ps1

--- AddMemberstoADGroup.ps1
+++ AddMemberstoADGroup.ps1
@@ -1,6 +1,8 @@
-﻿Import-Module ActiveDirectory
+# Import the Active Directory module to access AD cmdlets
+Import-Module ActiveDirectory
 #Define the name of your AD group Place in Quotes ""#
 $groupname =
 #Define Your members you want to add (Users & Groups) Ensure they are seperated by a comma#
 $Members =
+#Applies the arguments to the AD Group
 Add-ADGroupMember -Identity $groupname -Members $Members
